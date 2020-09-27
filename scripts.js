38d29a4934b75f80371aab15e9d741e04e61bb50
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-09-27 18:24:59 -0300

    falta fazer a funcionalidade do botão maximizar

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
-const modalOverlay = document.querySelector(".modal-overlay");
-const cards = document.querySelectorAll(".cards");
+const modalOverlay = document.querySelector(".modal-overlay")
+const cards = document.querySelectorAll(".card")
+const modal = document.querySelector(".modal")
 
-for (let card of cards) {
+for(let card of cards){
     card.addEventListener("click", function(){
         modalOverlay.classList.add("active")
-    })
+        const blogId = card.getAttribute("id")
+        modalOverlay.querySelector("iframe").src= `https://blog.rocketseat.com.br/${blogId}`
+    });
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
 })
+    
